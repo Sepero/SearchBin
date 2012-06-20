f1c6ac580ebb048228a886e080a31e4d1019ef95
a <a@EMONSTER.(none)>
2012-06-19 23:13:42 -0400

    Upgraded search function. Added plain text searching.

--- searchbin.py
+++ searchbin.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 
 """
+searchbin.py -t PATTERN [FILE [FILE...]]
 searchbin.py -p PATTERN [FILE [FILE...]]
 searchbin.py -f FILE [FILE [FILE...]]
 
 examples:
-./searchbin.py -p "0xCCDD??FF" myfile.exe
-Searches for the pattern "CCDD??FF" in myfile.exe, where ?? can be any byte value.
+./searchbin.py -t "hello" myfile.exe
+Searches for the text "hello" in myfile.exe.
+
+./searchbin.py -p "CCDDFF" myfile.exe
+Searches for the hexidecimal pattern "CCDDFF" in myfile.exe.
 
 ./searchbin.py -f pattern.bin myfile.exe
-Takes the binary file pattern.bin, and searches for an exact match within myfile.exe.
+Reads the file pattern.bin, and searches for a binary match within myfile.exe.
 
+Many more capabilites, just run ./searchbin.py --help
 
++Features: no compiling, fast, small file, wild card matches, search multiple files of unlimited size, all operating systems
 +Minimum Py2.7 required for argparse library
-+Features: no compiling, less code, files of unlimited size, similar in usage to grep
 +keywords binary grep search seek find fast
 
 license: BSD 2-Clause License, 2012, Sepero
 license: http://www.opensource.org/licenses/BSD-2-Clause
-http://github.com/Sepero/SearchBin
 """
 
-VERSION = "0.1"
+CONTACT=("sepero 111 @ gmail . com\n"
+         "https://github.com/Sepero/SearchBin/issues/new\n"
+         "http://seperohacker.blogspot.com/2012/04/binary-grep-program-searchbin.html")
 
-import sys, signal
+VERSION="0.20"
+
+import sys, signal, re
 
 def _exit_error(code, option="", err=None):
   """
@@ -31,14 +39,12 @@ def _exit_error(code, option="", err=None):
   Returns nothing.  All calls exit the program, error status 128.
   """
   error_codes = {
-    "2patterns":
-      "Cannot search for both a binary file and hex pattern. '-p -f'",
+    "Xpatterns":
+      "Cannot search for multiple patterns. '-t -p -f'",
     "0patterns":
-      "No pattern to search for was supplied. '-p -f'",
+      "No pattern to search for was supplied. '-t -p -f'",
     "decode":
       "The pattern string is invalid.\n" + str(option),
-    "nohex":
-      "The search pattern must start with '0x' for future compatibility",
     "bsize":
       "The buffer size must be at least %s bytes." % str(option),
     "sizes":
@@ -55,12 +61,11 @@ def _exit_error(code, option="", err=None):
       "Failed reading from file: %s" % option,
       
   }
-  if err: sys.stderr.write(str(err) + "\n")
-  sys.stderr.write("Error <" + str(code) + ">: " + error_codes[code] + "\n")
-  sys.stderr.write("Report issues to:\n")
-  sys.stderr.write("cowpie 2000 @ hotmail dot com\n")
-  sys.stderr.write("or http://github.com/Sepero/SearchBin\n\n")
-  sys.exit(128)
+  sys.stderr.write("version: %s\n" % VERSION)
+  sys.stderr.write("Report issues to: %s\n" % CONTACT)
+  if err: sys.stderr.write("%s\n" % str(err))
+  sys.stderr.write("Error <%s>: %s\n\n" % (code, error_codes[code]))
+  if __name__ == "__main__": sys.exit(128) # Exit under normal operation.
 
 
 def get_args():
@@ -69,37 +74,85 @@ def get_args():
   Returns an args object with attributes representing all arguments.
   """
   from argparse import ArgumentParser
-  description = "http://github.com/Sepero/SearchBin\nAn argument -f or -p is required. The -p argument accepts a hexidecimal pattern string and allows for missing characters, such as '0xFF??FF'. When using -f argument, the pattern file will be read as a binary file (not hex strings). If no search files are specified, %prog will read from standard input. The minimum memory required is about 3 times the size of the binary pattern. Increasing buffer-size will increase program search speed for large search files. All size arguments are read in decimal format, '-s 1024' = start searching after 1kilobyte. Reported finds are 0-based offset."
+  description = CONTACT +  """
+  An argument -t or -p or -f is required. The -p argument accepts a 
+  hexidecimal pattern string and allows for missing characters, 
+  such as 'FF??FF'. When using -f argument, the pattern file will 
+  be read as a binary file (not hex strings). If no search files are 
+  specified, %prog will read from standard input. The minimum memory 
+  required is about 3 times the size of the pattern byte length. 
+  Increasing buffer-size will increase program search speed for 
+  large search files. All size arguments (-b -s -e) are read in decimal 
+  format, for example: '-s 1024' will start searching after 1kilobyte.
+  Reported finds are 0-based offset.
+  """
   p = ArgumentParser(description=description)
   
   def add(s, **kwargs):
     args = s.split(":")
-    kwargs["dest"] = args.pop()
-    kwargs["metavar"] = args.pop()
-    kwargs["type"] = str if args.pop() else long ###
-    p.add_argument(*args, **kwargs)
     
-  add("-f:--file:string:FILE:fpattern",
+    value = args.pop() # pop last item.
+    if value:
+      kwargs["dest"] = value
+    value = args.pop()
+    if value:
+      kwargs["metavar"] = value
+    value = args.pop()
+    if value:
+      kwargs["type"] = eval(value) #(type)(value) # str(value) or long(value).
+    
+    p.add_argument(*args, **kwargs)
+  
+  add("-f:--file:str:FILE:fpattern",
       help = "file to read search pattern from")
-  add("-p:--pattern:long:PATTERN:pattern",
-      help = "a hexidecimal pattern in format '0xFF'")
+  add("-t:--text:str:PATTERN:tpattern",
+      help = "a (non-unicode case-sensitive) text string to search for")
+  add("-p:--pattern:str:PATTERN:ppattern",
+      help = "a hexidecimal pattern to search for")
   add("-b:--buffer-size:long:NUM:bsize",
-      help = "read buffer size. 8MB default")
+      help = "read buffer size (in bytes). 8MB default")
   add("-s:--start:long:NUM:start",
-      help = "starting position in file to begin searching")
+      help = "starting position in file to begin searching, as bytes")
   add("-e:--end:long:NUM:end",
       help = "end search at this position, measuring from beginning of file")
-  add("-m:--max-count:long:NUM:max",
-      help = "maximum number of matches to find")
-  add("-l:--log:string:FILE:log",
+  add("-m:--max-matches:long:NUM:max_matches",
+      help = "maximum number of matches to find (0=infinite)")
+  add("-l:--log:str:FILE:log",
       help = "write matched offsets to FILE, instead of standard output")
-  p.add_argument("fsearch", type=str, metavar = "FILE", nargs = "*",
-                 help = "files to search in for the pattern")
-  p.add_argument("-v", "--verbose", dest = "verbose", action = "store_true",
-                 help = "verbose, output the number of bytes searched after each buffer read")
-  p.add_argument("-V", "--version",  action = "version", 
-                 version = "%(prog)s " + VERSION)
+  add("str:FILE:fsearch", nargs = "*",
+      help = "files to search within")
+  add("-v:--verbose:::verbose", action = "store_true",
+      help = "verbose, output the number of bytes searched after each buffer read")
+  add("-V:--version:::", action = "version",
+      version = "%(prog)s " + VERSION)
+#  add("-d:--debug:::debug", action = "store_true",
+#      help = "debugging")
+      
   return p.parse_args()
+
+
+def hex_to_pattern(hex):
+  ret = []
+  pattern = hex
+  if hex[:2] == "0x": # Remove "0x" from start if it exists.
+    pattern = hex[2:]
+  try:
+    ret = [ p for p in pattern.split("??") ]
+    return [ p.decode("hex") for p in ret ]
+  except TypeError, e:
+    _exit_error("decode", hex, e)
+
+
+def text_to_pattern(text):
+  return [ t for t in text.split("?") ]
+
+
+def file_to_pattern(fname):
+  try: # If file specified, read it into memory.
+    with open(fname, "r") as f:
+      return [f.read()]
+  except IOError, e:
+    _exit_error("fpattern", fname, e)
 
 
 # We will be keeping the parsed args object and editing its attributes!
@@ -109,45 +162,38 @@ def verify_args(ar):
   Returns the modified args object.
   """
   # Make sure that exactly 1 pattern argument was given.
-  if ar.fpattern and ar.pattern:
-    _exit_error("2patterns")
-  elif ar.fpattern is None and ar.pattern is None:
+  all_patterns = filter(None, [ar.fpattern, ar.ppattern, ar.tpattern])
+  if len(all_patterns) > 1:
+    _exit_error("Xpatterns")
+  if len(all_patterns) == 0:
     _exit_error("0patterns")
   
-  # Change ar.pattern into a list, and fill it with binary string pieces.
+  # Create a new variable ar.pattern, and fill it with
+  # whichever pattern we have -t -f -p. ar.pattern will be a list.
   if ar.fpattern:
-    try: # If file specified, read it into memory.
-      with open(ar.fpattern, "r") as f:
-        ar.pattern = [ f.read() ]
-    except IOError, e:
-      _exit_error("fpattern", ar.fpattern, e)
-  else: # If not a file, split ar.patterns into searchable parts.
-    if ar.pattern[:2] != "0x": # (Literal string searching may be included in future version)
-      _exit_error("nohex")
-    ar.pattern = ar.pattern[2:]
-    try:
-      ar.pattern = [ p.decode("hex") for p in ar.pattern.split("??") ]
-    except TypeError, e:
-      _exit_error("decode", ar.pattern, e)
+    ar.pattern = file_to_pattern(ar.fpattern)
+  elif ar.tpattern:
+    ar.pattern = text_to_pattern(ar.tpattern)
+  else:
+    ar.pattern = hex_to_pattern(ar.ppattern)
   
   # Convert all number args from strings into long integers.
   try:
-    for attr in [ "bsize", "max", "start", "end" ]:
+    for attr in [ "bsize", "max_matches", "start", "end" ]:
       if getattr(ar, attr):
         setattr(ar, attr, long(getattr(ar, attr)))
   except ValueError, e:
     _exit_error("sizes", err=e)
   
-  # Buffer size must be at least double max pattern size.
+  # Buffer size must be at least double maximum pattern size.
   if ar.bsize:
     if ar.bsize < len("?".join(ar.pattern)) * 2:
       _exit_error("bsize", len("?".join(ar.pattern)) * 2)
   else:
     ar.bsize = len("".join(ar.pattern)) * 2
-    if ar.bsize < 2**23: # If bsize is < default, set to default.
-      ar.bsize = 2**23
-    
-  # End must be after start? :)
+    ar.bsize = max(ar.bsize, 2**23) # If bsize is < default, set to default.
+  
+  # End must be after start  :)
   if ar.start and ar.end and ar.start >= ar.end:
     _exit_error("startend")
   
@@ -161,88 +207,117 @@ def verify_args(ar):
   return ar
 
 
-def search(ar, fh):
+def search(ar, fh, debug=False):
+  """
+  This function is simply a wrapper to forward needed variables in a way
+  to make them all local variables. Local variables are faster than
+  accessing object attribute variables.
+  Returns nothing.
+  """
+  if not ar.debug:
+    _search_loop(ar.start, ar.end, ar.bsize, ar.pattern,
+                 ar.max_matches, ar.log, ar.verbose, fh.name,
+                 fh.read, fh.seek)
+  else:
+    _debug_search(ar.pattern, fh.name, fh.read)
+
+
+def _debug_search(pattern, fh_name, fh_read):
+  """
+  Slower, less functional, but less error prone search.
+  For debugging purposes.
+  Returns nothing.
+  """
+  len_pattern = len("?".join(pattern))
+  read_size = 2**24 - len_pattern # Amount to read each loop.
+  pattern = [ re.escape(p) for p in pattern ]
+  pattern = ".".join(pattern)
+  regex = re.compile(pattern, re.DOTALL+re.MULTILINE)
+  
+  try:
+    sbuffer = fh_read(len_pattern + read_size)
+    offset = 0
+    match = regex.search(sbuffer)
+    while True:
+      if not match:
+        offset += read_size
+        sbuffer = sbuffer[read_size:] # Erase front portion of buffer.
+        sbuffer += fh_read(read_size)
+        match = regex.search(sbuffer)
+      else:
+        print "Match at offset: %14d %12X in  %s" % (
+                      offset+match.start(), offset+match.start(), fh_name)
+        match = regex.search(sbuffer, match.start()+1)
+        
+      if len(sbuffer) <= len_pattern:
+        return
+  except IOError, e:
+    _exit_error("read", fh_name, e)
+
+
+def _search_loop(start, end, bsize, pattern, max_matches,
+                  log, verbose, fh_name, fh_read, fh_seek):
   """
   Searches the filehandler held by fh, with search settings in ar.
-  Returns nothing when finished.
+  Returns nothing.
   """
+  len_pattern = len("?".join(pattern)) # Byte length of pattern.
+  read_size = bsize - len_pattern # Amount to read each loop.
+  
+  # Convert pattern into a regular expression for insane fast searching.
+  pattern = [ re.escape(p) for p in pattern ]
+  pattern = ".".join(pattern)
+  # Grab regex search function directly to speed up function calls.
+  regex_search = re.compile(pattern, re.DOTALL+re.MULTILINE).search
+  
   # Set start reading position in file.
   try:
-    fh.seek(ar.start or 0)
+    fh_seek(start or 0)
   except IOError, e:
-    _exit_error("read", fh.name, err=e)
-  
-  # Localize variables for increased speed.
-  pattern = ar.pattern
-  verbose = ar.verbose
-  end = ar.end
-  len_pattern = len("?".join(pattern))
-  read_size = ar.bsize - len_pattern # Amount to read each loop.
-  match = False
-  find = 0
+    _exit_error("read", fh_name, err=e)
   
   try:
-    sbuffer = fh.read(len_pattern)
-    offset = long(ar.start or 0)
-    while True: # Begin main loop.
-      sbuffer += fh.read(read_size)
-      # If sbuffer is empty, or exceeded end argument, finish loop.
-      if len(sbuffer) <= len_pattern or (end and offset > end):
-        return
-      #print(len(sbuffer), "sbuffer") ## testing
-      if verbose: # Print each loop offset if verbose is on.
-        print("Passing offset: %14d %12X" % (offset, offset))
-      find = sbuffer.find(pattern[0], find) # Search first pattern.
-      #print(find) ##testing
-      # While pattern is found and if find is less than read_size (we have enough buffer for pattern to fully fit within it (else loop and read more).
-      while find != -1 and find < read_size:
-        if end and offset + find > end: # Found out beyond end, finish search.
+    offset = long(start or 0)
+    sbuffer = fh_read(len_pattern + read_size)
+    match = regex_search(sbuffer)
+    match = -1 if match == None else match.start()
+    while True:
+      if match == -1:
+        offset += read_size
+        # If end exists and we are beyond end, finish search.
+        if end and offset > end:
           return
-        find += 1 # Increment find for next search.
-        match = True # Set match True, but may be False if parts don't match.
-        # If pattern has multiple parts, then attempt to match remaining parts of pattern to the buffer. If they do not match, set match to False.
-        find2 = find + len(pattern[0]) # Use find2 for part matching loop.
-        for part in pattern[1:]:
-          #print(sbuffer[find2].encode("hex"), "hex sbuf") ## testing
-          #print(part.encode("hex"), "part") ## testing
-          if part and not sbuffer[find2:].startswith(part):
-            match = False # If any part doesn't match, flag sets False.
-            break
-          find2 += len(part) + 1 # +1 for each empty string which was ??.
-        #print(find, "find") ##testing
-        if not match: # If no match, continue searching.
-          find = sbuffer.find(pattern[0], find)
-        # Else match is True, and all parts of pattern were found.
-        else:
-          if ar.max:
-            ar.max -= 1
-          find_offset = offset + find - 1
-          s = "Match at offset: %14d %12X in  %s" % (
-                      find_offset, find_offset, fh.name)
-          if ar.log:
-            try:
-              ar.log.write(s + "\n")
-            except IOError, e:
-              _exit_error("logwrite", ar.log.name, e)
-          else:
-            print(s)
-            if ar.max == 0:
-              print("Found maximum number of matches.")
-          if ar.max == 0:
-            return
-        find = sbuffer.find(pattern[0], find)
+        sbuffer = sbuffer[read_size:] # Erase front portion of buffer.
+        sbuffer += fh_read(read_size)
+        match = regex_search(sbuffer)
+        # If there is no match set match to -1, else the matching position.
+        match = -1 if match == None else match.start()
+        if verbose: # Print each loop offset if verbose is on.
+          print("Passing offset: %14d %12X" % (offset, offset))
       else:
-        if find == -1:
-          find = read_size
-      #print(len(sbuffer), find) ## testing
-      
-      offset += read_size # Increase offet by amount to read.
-      sbuffer = sbuffer[read_size:] # Erase front portion of buffer.
-      find = find - read_size # Shift find index for the shifted buffer.
-      #print(len(sbuffer), find) ## testing
-      #assert find >= 0 ## testing
+        # If end exists and we are beyond end, finish search.
+        if match == -1 and offset + match > end:
+          return
+        
+        # Print matched offset.
+        find_offset = offset + match
+        print "Match at offset: %14d %12X in  %s" % (
+                      find_offset, find_offset, fh_name)
+        
+        if max_matches:
+          max_matches -= 1
+          if max_matches == 0: # If maximum matches are found, then end.
+            print("Found maximum number of matches.")
+            return
+        
+        # Get next match.
+        match = regex_search(sbuffer, match+1)
+        match = -1 if match == None else match.start()
+        
+      if len(sbuffer) <= len_pattern: # Finished reading file- end.
+        return
   except IOError, e:
-    _exit_error("read", fh.name, e)
+    _exit_error("read", fh_name, e)
 
 
 def main():
@@ -266,3 +341,4 @@ if __name__ == "__main__":
     sys.exit()
   signal.signal(signal.SIGINT, exit)
   main()
+
