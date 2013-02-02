bacdeb8a38c585446d547d3cc19db8adebd40b45
Sepero <cowpie2000@hotmail.com>
2013-02-02 10:30:45 -0500

    Switched to universal tab indentation
    Switched to universal tab indentation. Also added minor tweets to testing.

--- unittest/unit_test.py
+++ unittest/unit_test.py
@@ -5,45 +5,53 @@ sys.path.append("..")
 import searchbin as s
 
 class MyTest(unittest.TestCase):
-  def __init__(self):
-    super.__init__(self)
-    self.randoms = []
-    for i in xrange(1000):
-      num = random.randint(0, 1000000000)
-      self.randoms.append("%X" % num)
-  
-  def setUp(self):
-    random.seed()
-  
-  def test_hex(self):
-    for i in xrange(1000):
-      num = random.randint(0, 1000000000)
-      str1 = "%X" % num
-      if len(str1)%2 != 0:
-        str1 = "0" + str1
-      str2 = "0x" + str1
-      self.assertEqual(s.hex_to_pattern(str1)[0], str1.decode("hex"))
-      self.assertEqual(s.hex_to_pattern(str2)[0], str1.decode("hex"))
-      
-      r = random.randint(0, 5) * 2
-      if len(str1) > r:
-        str1 = str1[:r] + "??" + str1[r:]
-      
-      self.assertTrue(s.hex_to_pattern(str1))
-      
-      try:
-        s.hex_to_pattern(str(random.randint(0,1000000)).decode("hex")+str2)
-        self.assertFalse(True) # This statement should never be reached.
-      except:
-        pass
-  
-  def test_text(self):
-    pass
-  
-  def tearDown(self):
-    pass
+	def setUp(self):
+		random.seed()
+		#self.randoms = []
+		#for i in xrange(1000):
+			#num = random.randint(0, 1000000000)
+			#self.randoms.append("%X" % num)
+	
+	def test_hex(self):
+		"""Test the hex_to_pattern() function"""
+		for i in xrange(1000):
+			num = random.randint(0, 1000000000)
+			# Turn random number into a hex string.
+			hex_str1 = "%X" % num
+			
+			# Make sure the random hex string is an even number of characters long."
+			if len(hex_str1)%2 != 0:
+				hex_str1 = "0" + hex_str1
+			
+			# Test against plain hex, and hex starting with "0x".
+			hex_str2 = "0x" + hex_str1
+			
+			self.assertEqual(s.hex_to_pattern(hex_str1)[0], hex_str1.decode("hex"))
+			self.assertEqual(s.hex_to_pattern(hex_str2)[0], hex_str1.decode("hex"))
+			
+			# Create a random pattern with a few "??" in it.
+			r = random.randint(0, 5) * 2
+			if len(hex_str1) > r:
+				hex_str1 = hex_str1[:r] + "??" + hex_str1[r:]
+			
+			# Make sure the pattern can match the random number.
+			self.assertTrue(s.hex_to_pattern(hex_str1))
+			
+			try:
+				# Make sure this fails on error.
+				s.hex_to_pattern(str(random.randint(0,1000000)).decode("hex")+hex_str2)
+				self.assertFalse(True) # This statement should never be reached.
+			except:
+				pass
+	
+	def test_text(self):
+		pass
+	
+	def tearDown(self):
+		pass
 
 
 if __name__ == '__main__':
-  unittest.main()
+	unittest.main() #import ipdb; ipdb.set_trace()
+	
 
